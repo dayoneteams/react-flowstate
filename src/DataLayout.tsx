e662233cb81128d37f4b82a9ea20663bda80fce8
Hao Tang <haotang.io@gmail.com>
2023-04-21 11:50:29 +0700

    feat: add tsdx

--- src/DataLayout.tsx
+++ src/DataLayout.tsx
@@ -1,3 +1,41 @@
-export const DataLayout = () => {
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useState
+} from 'react';
+import {LoadingIndicator} from './LoadingIndicator';
 
+type Props<D> = {
+  children: (data: D) => ReactNode;
+  refresh?: (method: {refetchData: () => void}) => void;
+  fetchFn: () => D | Promise<D>;
+};
+export const SmartLayoutCtx = createContext<{data?: any}>({});
+export const DataLayout = <D,>({fetchFn, refresh, children}: Props<D>) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<D>();
+
+  const findData = async () => {
+    const fetchedData = await fetchFn();
+    setData(fetchedData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    findData();
+  }, [fetchFn, refresh]);
+
+  if (refresh) {
+    refresh({refetchData: findData});
+  }
+
+  const renderChildren = useCallback(() => children(data), [data, children]);
+
+  return (
+    <SmartLayoutCtx.Provider value={{data}}>
+      {loading ? <LoadingIndicator /> : renderChildren()}
+    </SmartLayoutCtx.Provider>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,6 @@
-export * from './DataLayout';
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  return <div>the snozzberries taste like snozzberries</div>;
+};
