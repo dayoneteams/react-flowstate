7a946240e16578e28bb1fbc3e11fbac90d916787
Hao Tang <haotang.io@gmail.com>
2023-04-22 17:07:34 +0700

    feat: update examples

--- examples/index.tsx
+++ examples/index.tsx
@@ -1,17 +1,18 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {fetchData} from "./data";
+import {fetchData} from './data';
 import {DataLayout} from '../.';
 import "./style.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Appreciate Great Works from Great Developers</h1>
+      <h1><center>Awesome React Libraries</center></h1>
+      <h3><center>Appreciate Great Works</center></h3>
       <DataLayout
         fetchFn={fetchData}
-        loadingIndicator={() => <div className="loading-indicator">Loading...</div>}
+        loadingIndicator={() => <div className="loading-indicator">Wait me a sec ...</div>}
       >
         {({ data , reload }) => (
           <div className="container">
