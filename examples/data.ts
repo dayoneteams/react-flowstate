d3227b248cb44e91b7fc9d3f04df3b73e9b89021
Hao Tang <haotang.io@gmail.com>
2023-04-24 15:50:16 +0700

    style: add lint fix for examples

--- examples/data.ts
+++ examples/data.ts
@@ -4,58 +4,67 @@ const SIZE = 4;
 const DELAY = 2000;
 
 const REACT_LIBRARIES = [
-    {
-      "name": "React Router",
-      "description": "A library for building single-page applications with dynamic routing in React.",
-      "websiteUrl": "https://reactrouter.com/"
-    },
-    {
-      "name": "Redux",
-      "description": "A predictable state container for JavaScript apps, providing a centralized store for managing application state.",
-      "websiteUrl": "https://redux.js.org/"
-    },
-    {
-      "name": "Material-UI",
-      "description": "A library of React components that implement Google's Material Design specification, offering pre-designed components for building responsive and customizable UIs.",
-      "websiteUrl": "https://mui.com/"
-    },
-    {
-      "name": "Next.js",
-      "description": "A framework for building server-rendered React applications, offering features like automatic code splitting and server-side rendering.",
-      "websiteUrl": "https://nextjs.org/"
-    },
-    {
-      "name": "Styled Components",
-      "description": "A library for styling React components with CSS-in-JS syntax, allowing developers to write CSS directly in their JavaScript code.",
-      "websiteUrl": "https://styled-components.com/"
-    },
-    {
-      "name": "Formik",
-      "description": "A library for building forms in React, offering a simple API for managing form state and validation.",
-      "websiteUrl": "https://formik.org/"
-    },
-    {
-      "name": "React Native",
-      "description": "A framework for building mobile applications with React, allowing developers to build cross-platform mobile apps with JavaScript and React.",
-      "websiteUrl": "https://reactnative.dev/"
-    },
-    {
-      "name": "React Testing Library",
-      "description": "A library for testing React components, offering a set of utilities for rendering components and asserting their behavior.",
-      "websiteUrl": "https://testing-library.com/docs/react-testing-library/intro/"
-    },
-    {
-      "name": "Axios",
-      "description": "A library for making HTTP requests from JavaScript applications, offering a simple API for sending and receiving data from APIs.",
-      "websiteUrl": "https://axios-http.com/"
-    },
-    {
-      "name": "React Bootstrap",
-      "description": "A library of React components that implement Bootstrap's CSS framework, offering pre-designed components for building responsive and customizable UIs.",
-      "websiteUrl": "https://react-bootstrap.github.io/"
-    }
-  ]
-;
+  {
+    name: 'React Router',
+    description:
+      'A library for building single-page applications with dynamic routing in React.',
+    websiteUrl: 'https://reactrouter.com/',
+  },
+  {
+    name: 'Redux',
+    description:
+      'A predictable state container for JavaScript apps, providing a centralized store for managing application state.',
+    websiteUrl: 'https://redux.js.org/',
+  },
+  {
+    name: 'Material-UI',
+    description:
+      "A library of React components that implement Google's Material Design specification, offering pre-designed components for building responsive and customizable UIs.",
+    websiteUrl: 'https://mui.com/',
+  },
+  {
+    name: 'Next.js',
+    description:
+      'A framework for building server-rendered React applications, offering features like automatic code splitting and server-side rendering.',
+    websiteUrl: 'https://nextjs.org/',
+  },
+  {
+    name: 'Styled Components',
+    description:
+      'A library for styling React components with CSS-in-JS syntax, allowing developers to write CSS directly in their JavaScript code.',
+    websiteUrl: 'https://styled-components.com/',
+  },
+  {
+    name: 'Formik',
+    description:
+      'A library for building forms in React, offering a simple API for managing form state and validation.',
+    websiteUrl: 'https://formik.org/',
+  },
+  {
+    name: 'React Native',
+    description:
+      'A framework for building mobile applications with React, allowing developers to build cross-platform mobile apps with JavaScript and React.',
+    websiteUrl: 'https://reactnative.dev/',
+  },
+  {
+    name: 'React Testing Library',
+    description:
+      'A library for testing React components, offering a set of utilities for rendering components and asserting their behavior.',
+    websiteUrl: 'https://testing-library.com/docs/react-testing-library/intro/',
+  },
+  {
+    name: 'Axios',
+    description:
+      'A library for making HTTP requests from JavaScript applications, offering a simple API for sending and receiving data from APIs.',
+    websiteUrl: 'https://axios-http.com/',
+  },
+  {
+    name: 'React Bootstrap',
+    description:
+      "A library of React components that implement Bootstrap's CSS framework, offering pre-designed components for building responsive and customizable UIs.",
+    websiteUrl: 'https://react-bootstrap.github.io/',
+  },
+];
 
 export const fetchData = () =>
   new Promise(resolve =>
--- examples/index.tsx
+++ examples/index.tsx
@@ -1,23 +1,24 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {fetchData} from './data';
-import {DataLayout} from '../.';
+import { fetchData } from './data';
+import { DataLayout } from '../.';
 
 const App = () => {
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto p-4">
       <h1 className="text-3xl text-center">Awesome React Libraries</h1>
       <h3 className="text-1xl text-center mb-5">Appreciate the Great Works</h3>
       <DataLayout
         fetchFn={fetchData}
-        loadingIndicator={() =>
+        loadingIndicator={() => (
           <div className="text-center">
             <progress className="progress progress-accent w-56" />
             <div>Wait me a sec ...</div>
-          </div>}
+          </div>
+        )}
       >
-        {({ data , reload, isLoadingInShadow }) => (
+        {({ data, reload, isLoadingInShadow }) => (
           <div>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
               {data.map((member, index) => (
@@ -25,15 +26,27 @@ const App = () => {
                   <div className="card-body">
                     <h2 className="card-title">{member.name}</h2>
                     <p>{member.description}</p>
-                    <a className="link link-secondary" href={member.websiteUrl} target="_blank">{member.websiteUrl}</a>
+                    <a
+                      className="link link-secondary"
+                      href={member.websiteUrl}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {member.websiteUrl}
+                    </a>
                   </div>
                 </div>
               ))}
             </div>
             <div className="mt-5 flex justify-center">
-              <button onClick={() => reload()} className="btn btn-secondary">Reload</button>
+              <button onClick={() => reload()} className="btn btn-primary">
+                Reload
+              </button>
               &nbsp;
-              <button onClick={() => reload({shadow: true})} className="btn btn-secondary">
+              <button
+                onClick={() => reload({ shadow: true })}
+                className="btn btn-secondary"
+              >
                 {isLoadingInShadow ? 'Reloading ...' : 'Shadow Reload'}
               </button>
             </div>
