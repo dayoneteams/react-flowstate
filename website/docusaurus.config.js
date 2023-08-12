cb2661841f2c5136830baed784d9bc136fdc413c
Hao Tang <haotang.io@gmail.com>
2023-08-13 00:42:34 +0700

    docs: update config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -14,12 +14,13 @@ const config = {
   url: 'https://dayoneteams.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/react-flowstate',
+  baseUrl: '/react-flowstate/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'dayoneteams', // Usually your GitHub org/user name.
+  projectName: 'react-flowstate', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
